9a1ef8d328b3875b1d0126989bbceaf7338eb72a
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-07-17 12:28:09 -0500

    indexImage

--- src/mainPIGUI.jl
+++ src/mainPIGUI.jl
@@ -1,5 +1,10 @@
+# Path to index logo
+const indexImgPath = joinpath(dirname(Base.source_path()), "\\figures\\index_logo.png")
+
 function PIAssistantGUI()
 
-    println("Works!")
+    indexImg = Image()
+    set_gtk_property!(indexImg, :file, indexImgPath)
 
+    Gtk.showall(indexImg)
 end
