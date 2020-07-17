c9b9f3ade34f82ae2c8d034d46e4a1272f110166
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-07-16 19:03:29 -0500

    Developers

--- src/PIAssistant.jl
+++ src/PIAssistant.jl
@@ -1,5 +1,10 @@
 module PIAssistant
-
+    # Developed by:
+    # PhD. Arick Castillo Landero
+    # PhD. Arturo Jimenez Gutierrez
+    # PhD. Kelvyn B. Sánchez Sánchez
+    # Instituto Tecnológico de Celaya/TecNM - México
+    
     # Function to call the GUI
     export PIAssistantGUI()
 
