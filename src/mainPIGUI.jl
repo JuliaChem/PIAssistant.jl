f5f0972a79d73da85d1897aab69cbe7888214f9a
Kelvyn Baruc Sánchez Sánchez <kelvyn.baruc@gmail.com>
2020-07-17 15:56:41 -0500

    Fix issue with w , h

--- src/mainPIGUI.jl
+++ src/mainPIGUI.jl
@@ -1,18 +1,28 @@
 using Gtk.ShortNames, DataFrames, DefaultApplication, Mustache, Luxor
 
 # Path to index logo
-global indexImgPath = joinpath(dirname(Base.source_path()), "figures\\index_logo.png")
-global indexImgPathT = joinpath(dirname(Base.source_path()), "C:\\Windows\\Temp\\index_logo_temp.png")
+if Sys.iswindows()
+    global indexImgPath =
+        joinpath(dirname(Base.source_path()), "figures\\index_logo.png")
+    global indexImgPathT =
+        joinpath(dirname(Base.source_path()), "C:\\Windows\\Temp\\index_logo_temp.png")
+end
+
+if Sys.islinux()
+    global indexImgPath =
+        joinpath(dirname(Base.source_path()), "figures/index_logo.png")
+    global indexImgPathT =
+        joinpath(dirname(Base.source_path()), "/temp/index_logo_temp.png")
+end
 
 function PIAssistantGUI()
-    global w, h = screen_size()
+    global ws, hs = screen_size()
 
     # Creates a temporary Image as index.
-    Drawing(round(w/3.5), round(h/2.5), indexImgPathT)
+    Drawing(round(ws/3.5), round(hs/2.5), indexImgPathT)
 
     origin()
     background("white")
-
 
     img = readpng(indexImgPath)
     w = img.width
@@ -31,8 +41,8 @@ function PIAssistantGUI()
     set_gtk_property!(winIndex, :decorated, false)
     set_gtk_property!(winIndex, :window_position, 3)
     set_gtk_property!(winIndex, :accept_focus, true)
-    set_gtk_property!(winIndex, :width_request, round(w/3.5))
-    set_gtk_property!(winIndex, :height_request, round(h/2.5))
+    set_gtk_property!(winIndex, :width_request, round(ws/3.5))
+    set_gtk_property!(winIndex, :height_request, round(hs/2.5))
 
     Gtk.showall(winIndex)
 
