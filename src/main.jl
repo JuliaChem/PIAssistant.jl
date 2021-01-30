3cd4b059e44deaae634c0495b4c174df5679c51b
Kelvyn <kelvyn.baruc@gmail.com>
2021-01-29 23:07:45 -0600

    Fix Linux 2

--- src/main.jl
+++ src/main.jl
@@ -433,7 +433,7 @@ function mainPI()
     if Sys.iswindows()
         set_gtk_property!(equipmentFrameTree, :height_request, (hNb - 30)/2 - 110)
     else
-        set_gtk_property!(equipmentFrameTree, :height_request, (hNb - 30)/2 - 100)
+        set_gtk_property!(equipmentFrameTree, :height_request, (hNb - 30)/2 - 103)
     end
     set_gtk_property!(equipmentFrameTree, :width_request, wBC - 20)
     set_gtk_property!(equipmentFrameTree, :margin_top, 5)
